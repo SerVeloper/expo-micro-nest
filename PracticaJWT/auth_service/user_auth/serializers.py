ef20ca52cf8b8b80538e646db3599d6d971ec169
Valeria Alexandra Cuellar Coca <142897380+valeriaalexandracuellarcoca@users.noreply.github.com>
2025-10-09 13:16:05 -0300

    Update serializers.py

--- PracticaJWT/auth_service/user_auth/serializers.py
+++ PracticaJWT/auth_service/user_auth/serializers.py
@@ -57,6 +57,4 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['roles'] = [group.name for group in user.groups.all()]
-        token['username'] = user.username
-        token['email'] = user.email
         return token
