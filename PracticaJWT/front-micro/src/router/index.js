91cc6eeeae577e74049900a4049da35733936b60
seVeloper <sergioveloper@gmail.com>
2025-10-16 10:39:18 -0400

    correccion rutas

--- PracticaJWT/front-micro/src/router/index.js
+++ PracticaJWT/front-micro/src/router/index.js
@@ -28,7 +28,19 @@ const routes = [
     name: 'Client',
     component: PanelClient,
     meta: { requiresAuth: true },
-    
+    children: [
+      {
+        path: 'events',
+        name: 'ClientEvents',
+        component: () => import('@/views/EventsClient.vue')
+      },
+      {
+        path: 'events/:id',
+        name: 'ClientEventDetail',
+        component: () => import('@/components/client/EventDetail.vue'),
+        props: true
+      }
+    ]
   }
 ]
 
